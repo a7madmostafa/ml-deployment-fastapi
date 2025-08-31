a396bf1f00ef8ca53de655071091eb6bbb745838
Ahmad Mostafa <a.m_eg@yahoo.com>
2025-08-31 05:58:35 +0000

    first commit

---
+++ train.py
@@ -0,0 +1,76 @@
+
+
+import pandas as pd
+import numpy as np
+import pickle
+
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import make_pipeline
+
+def load_data():
+    data_url = 'https://raw.githubusercontent.com/alexeygrigorev/mlbookcamp-code/master/chapter-03-churn-prediction/WA_Fn-UseC_-Telco-Customer-Churn.csv'
+
+    df = pd.read_csv(data_url)
+
+    df.columns = df.columns.str.lower().str.replace(' ', '_')
+
+    categorical_columns = list(df.dtypes[df.dtypes == 'object'].index)
+
+    for c in categorical_columns:
+        df[c] = df[c].str.lower().str.replace(' ', '_')
+
+    df.totalcharges = pd.to_numeric(df.totalcharges, errors='coerce')
+    df.totalcharges = df.totalcharges.fillna(0)
+
+    df.churn = (df.churn == 'yes').astype(int)
+    return df
+
+
+def train_model(df):
+    y_train = df.churn
+
+    numerical = ['tenure', 'monthlycharges', 'totalcharges']
+    categorical = [
+        'gender',
+        'seniorcitizen',
+        'partner',
+        'dependents',
+        'phoneservice',
+        'multiplelines',
+        'internetservice',
+        'onlinesecurity',
+        'onlinebackup',
+        'deviceprotection',
+        'techsupport',
+        'streamingtv',
+        'streamingmovies',
+        'contract',
+        'paperlessbilling',
+        'paymentmethod',
+    ]
+
+    train_dict = df[categorical + numerical].to_dict(orient='records')
+
+    pipeline = make_pipeline(
+        DictVectorizer(),
+        LogisticRegression(solver='liblinear')
+    )
+
+    pipeline.fit(train_dict, y_train)
+    print(' model training completed ')
+    return pipeline
+
+def save_model(pipeline, output_file='model.bin'):
+    with open(output_file, 'wb') as f_out:
+        pickle.dump(pipeline, f_out)
+    print(f' model saved to {output_file} ')
+
+def main():
+    df = load_data()
+    pipeline = train_model(df)
+    save_model(pipeline)
+
+
+if __name__ == '__main__':
+    main()
